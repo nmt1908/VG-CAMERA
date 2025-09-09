63772473abdb4beae9e16221173d7273411fc26d
nmt1908 <beosex11223@gmail.com>
2025-09-09 07:31:34 +0700

    fix error api

--- app/src/main/java/com/example/vgcamera/MainActivity.java
+++ app/src/main/java/com/example/vgcamera/MainActivity.java
@@ -762,7 +762,7 @@ public class MainActivity extends AppCompatActivity implements FaceAnalyzer.Face
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults); 
         if (requestCode == 101 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             startCamera();
         }
